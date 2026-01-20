d3d73e54bea25cf7019654dc940644bb585ee929
akshithaa-23 <akshithareddyk23@gmail.com>
2026-01-20 10:46:45 +0530

    Angular Assignment -1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from '../components/nav-bar/nav-bar';
+import { Description } from '../components/description/description';
+import { WelcomeBanner } from '../components/welcome-banner/welcome-banner';
+import { Profiles } from '../components/profiles/profiles';
+import { Footer } from '../components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavBar,Description,WelcomeBanner,Profiles,Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
