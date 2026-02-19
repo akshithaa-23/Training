6558486193af0715452564ea8617df6a198a4a0f
akshithaa-23 <akshithareddyk23@gmail.com>
2026-02-19 15:37:56 +0530

    JWT1

---
+++ DOTNET/JWTAuthentication/BasicAuthentication/Controllers/AuthController.cs
@@ -0,0 +1,120 @@
+﻿using BasicAuthentication.DTO;
+using BasicAuthentication.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicAuthentication.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthContext authContext;
+        public AuthController(AuthContext authContext)
+        {
+            this.authContext = authContext;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserDTO user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var newUser = authContext.Users.FirstOrDefault(u => u.Email == user.Email);
+            if (newUser != null)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                var userToAdd = new User
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Email = user.Email,
+                    Password = user.Password,
+                    isActive = true,
+                    date = DateTime.Now
+                };
+                authContext.Users.Add(userToAdd);
+                authContext.SaveChanges();
+                return Ok(userToAdd);
+            }
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginDTO user)
+        {
+            if (!ModelState.IsValid)
+            {
+                BadRequest(ModelState);
+            }
+            var objUser = authContext.Users.FirstOrDefault(u => u.Email == user.Email && u.Password == user.Password);
+            if (objUser != null)
+            {
+                return Ok("Login successful");
+            }
+            else
+            {
+                return BadRequest("Invalid email or password");
+            }
+        }
+
+        [HttpGet]
+        [Route("getAllUsers")]
+        public IActionResult GetAllUsers()
+        {
+            var users = authContext.Users.ToList();
+
+            return Ok(users);
+
+        }
+
+        [HttpGet("{id}")]
+    
+        public IActionResult GetAllUsersID(int id)
+        {
+            var users = authContext.Users.FirstOrDefault(u=>u.UserId==id);
+            if(User == null)
+            {
+                return NotFound("User not found");
+            }
+            return Ok(users);
+
+        }
+        [HttpGet("{Email}")]
+        public IActionResult GetByEmail(string email)
+        {
+            var users=authContext.Users.FirstOrDefault(u => u.Email == email);
+            if (users == null) { return NotFound("User not found"); }
+                        return Ok(users);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ DOTNET/JWTAuthentication/BasicAuthentication/Models/AuthContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BasicAuthentication.Models
+{
+    public class AuthContext :DbContext
+    {
+        public AuthContext(DbContextOptions<AuthContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    
+    }
+}
